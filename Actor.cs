a41e286be2ef49d341f0c80642243d3eba9d7a94
heukhyeon <heukhyeon@gmail.com>
2017-06-08 22:14:31 +0900

    2017-06-08
    정적 매니저 클래스 작성.

---
+++ Actor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// 플레이어, 적, NPC등 캐릭터들에 대한 공통 상속 클래스
+/// </summary>
+public class Actor : CustomObject
+{
+    /// <summary>
+    /// 기본 스테이터스. 추후 더 추가예정
+    /// </summary>
+    public struct Status
+    {
+        public int Hp;
+        public int Mp;
+        public int Atk;
+    }
+    /// <summary>
+    /// 현재 객체의 타입을 나타낸다. 해당값에 따라 처리가 달라지게 할 예정.
+    /// </summary>
+    public enum CharacterType { Player,Enemy,NPC};
+}
+
---
+++ CustomObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/// <summary>
+/// 대부분의 객체에 기본적으로 탑재되는 Monobehavior아래 최상위 상속클래스. 
+/// </summary>
+public class CustomObject : MonoBehaviour {
+
+    private void Awake()
+    {
+        SceneObjectManager.Add(this.gameObject);
+    }
+
+}
+
+
